2a1f6f10c560668781c1923a7afa124c00c8cb86
Michael King <rgkjhshi@gmail.com>
2014-11-19 13:09:41 +0800

    添加skea部分接口

--- skea/handler.py
+++ skea/handler.py
@@ -13,330 +13,149 @@ import os
 import db
 
 #
-# 反馈信息代码说明
+# 状态信息代码说明
 
 # 100: 成功
 # 101: 参数错误
 # 102: 该用户已被注册
 # 103: 用户名或密码错误
+# 104: 结果未找到
 
 class RegisterHandler(tornado.web.RequestHandler):
     def post(self):
         username = self.get_argument('email', '')
         password = self.get_argument('password', '')
         nickname = self.get_argument('nickname', '')
-        data = {'status':False, 'messageCode':101}
+        data = {'status':101}
         if username == '' or password == '' or nickname == '':
-            data['messageCode'] = 101
+            data['status'] = 101
         else :
             sql = "select * from `ofUser` where username=? "
             res = db.select_one(sql, username)  # 查询数据库是否已经被注册过了
             if res is not None :                # 被注册过，status设为失败，并设置已被注册的信息
-                data['messageCode'] = 102
+                data['status'] = 102
             else :                              # 未被注册过，进行注册
                 table = "ofUser"
-                kw = {"username":username, "password":password}
+                kw = {"username":username, "password":password, "nickname":nickname}
                 res = db.insert(table, **kw)
-                data['status'] = True
-                data['messageCode']    = 100
+                data['status']    = 100
         json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
         self.write(json_result)
 
 
 class LoginHandler(tornado.web.RequestHandler):
     def post(self):
-        username = self.get_argument('email')
-        password = self.get_argument('pwd')
-        data = {'status':False}
+        username = self.get_argument('email', '')
+        password = self.get_argument('password', '')
+        data = {'status':103}
         sql = "select * from `ofUser` where username=? and password=? "
         res = db.select_one(sql, username, password)   # 查询数据库，看用户名和密码是否正确
         if res is None :              # 未找到，则用户名或密码不对
             pass
         else :                         # 找到，则用户名密码正确；此时可以确保本次登录成功。
-            data['status'] = True
-            self.set_secure_cookie("username", username)
+            data['status'] = 100
         json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
         self.write(json_result)
 
-class LogoutHandler(tornado.web.RequestHandler):
-    def post(self):
-        username = self.get_secure_cookie("username")
-        # 检查是否登录
-        if username is None:
-            data = {'status':False, 'msg':'您尚未登录'}
-            json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-            self.write(json_result)
-            return
-        data = {'status':True, 'msg':'注销成功'}
-        self.clear_cookie("username")
-        json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-        self.write(json_result)
 
-class EditInfoHandler(tornado.web.RequestHandler):
+class EditNicknameHandler(tornado.web.RequestHandler):
     def post(self):
-        # 获取cookie保证用户已登录
-        username = self.get_secure_cookie("username")
-        # 检查是否登录
-        if username is None:
-            data = {'status':False, 'msg':'您尚未登录'}
-            json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-            self.write(json_result)
-            return
         # 获取post数据
+        username = self.get_argument('email', '')
         nickname = self.get_argument('nickname', '')
-        gender   = self.get_argument('gender', '男')
-        age      = self.get_argument('age', '18')
-        height   = self.get_argument('height', '')
-        weight   = self.get_argument('weight', '')
         # 默认为修改失败
-        data = {'status':False, 'msg':'修改失败'}
+        data = {'status':101, }
         # 构造修改语句
-        sql = "update `ofUser` set nickname=?, gender=?, age=?, height=?, weight=? where username=? "  
+        sql = "update `ofUser` set nickname=? where username=? "  
         # 修改
-        res = db.update(sql, nickname, gender, int(age), height, weight, username)
+        res = db.update(sql, nickname, username)
         if res :       # 修改成功
-            data['status'] = True
-            data['msg']    = '修改成功'
+            data['status'] = 100
+        else:
+            data['status'] = 104
         json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
         self.write(json_result)
 
 
 class GetInfoHandler(tornado.web.RequestHandler):
     def post(self):
-        username = self.get_secure_cookie("username")
-        # 检查是否登录
-        if username is None:
-            data = {'status':False, 'msg':'您尚未登录'}
-            json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-            self.write(json_result)
-            return
-        data = {'status':False, 'msg':'获取失败'}
+        username = self.get_argument('email', '')
+        data = {'status':101 }
         sql = "select * from `ofUser` where username=? "
         res = db.select_one(sql, username)
         if res :                         # 查找成功
-            data['status'] = True
-            data['msg']    = '获取成功'
+            data['status'] = 100
             info = {}
             info['username'] = username
             if res['nickname']:
-                info['nickname'] = res['nickname'].encode("utf8")
-            else :
-                info['nickname'] = res['nickname']
-            if res['gender']:
-                info['gender']   = res['gender'].encode("utf8")
-            else :
-                info['gender']   = res['gender']
-            # age为int，不需要编码
-            info['age']      = res['age']
+                info['nickname'] = res['nickname']#.encode("utf8")
+            if res['birthday']:
+                info['birthday'] = res['birthday']
             if res['height']:
-                info['height']   = res['height'].encode("utf8")
-            else :
                 info['height']   = res['height']
             if res['weight']:
-                info['weight']   = res['weight'].encode("utf8")
-            else :
                 info['weight']   = res['weight']
-            if res['avatar']:
-                info['avatar']   = res['avatar'].encode("utf8")
-            else :
-                info['avatar']   = res['avatar']
             data['info']     = info
-        json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
+        else:
+            data['status'] = 104
+        json_result = json.dumps(data, ensure_ascii=False)     # 把python对象编码成json格式的字符串
         self.write(json_result)
 
-class ChageAvatarHandler(tornado.web.RequestHandler):
+
+class SaveQuestionResultHandler(tornado.web.RequestHandler):
     def post(self):
-        # 获取cookie保证用户已登录
-        username = self.get_secure_cookie("username")
-        # 检查是否登录
-        if username is None:
-            data = {'status':False, 'msg':'您尚未登录'}
-            json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-            self.write(json_result)
-            return
+        username = self.get_argument('email', '')
+        birthday = self.get_argument('birthday', '')
+        height   = self.get_argument('height', '')
+        weight   = self.get_argument('weight', '')
+
+        date     = self.get_argument('date', '')
+        result   = self.get_argument('result', '')
         # 默认为修改失败
-        data = {'status':False, 'msg':'修改失败'}
-        # request.files会有这种格式的信息:{"avatar":[{'filename':'', 'content_type':'', 'body':''}]}
-        # filename为客户端的文件名字; content_type为MIME类型，如image/gif, image/jpeg, image/png等; body为文件内容
-        if self.request.files == {} or 'avatar' not in self.request.files: # 没有所需上传文件
-            data['msg']    = '上传文件失败'
-        else :
-            # 常用的图片格式有：image/jpeg，image/bmp，image/pjpeg，image/gif，image/x-png，image/png
-            image_type_list = ['image/gif', 'image/jpeg', 'image/pjpeg', 'image/bmp', 'image/png', 'image/x-png']
-            # 得到文件对象
-            send_file = self.request.files['avatar'][0]
-            # 限制文件格式
-            if send_file['content_type'] not in image_type_list:
-                data['msg'] = '请上传jpeg、gif、png、bmp等常见格式的图片'
-            # 限制文件大小
-            elif len(send_file['body']) > 4 * 1024 * 1024:
-                data['msg'] = '请上传4M以下的图片'
-            # 满足所有要求,存储图片
-            else:
-                # 存储也就是将send_file['body']内容进行存储，type(send_file['body'])为str
-                # 先将文件写入临时文件，然后再用PIL对这个临时文件进行处理。
-                tmp_file = tempfile.NamedTemporaryFile(delete=True) #创建临时文件，当文件关闭时自动删除
-                tmp_file.write(send_file['body'])  #写入临时文件
-                tmp_file.seek(0)   #将文件指针指向文件头部，因为上面的操作将指针指向了尾部
-                #此时用PIL再处理进行存储，PIL打开不是图片的文件会出现IOERROR错误，这就可以识别后缀名虽然是图片格式，但内容并非是图片。
-                try:
-                    image_one = Image.open( tmp_file.name )
-                except IOError, error:
-                    tmp_file.close()
-                    data['msg'] = '图片格式不合法'
-                    # 出错直接返回
-                    json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-                    self.write(json_result)
-                    return
-                # 判断图片尺寸，不在尺寸内拒绝操作
-                #if image_one.size[0] < 250 or image_one.size[1] < 250 or image_one.size[0] > 2000 or image_one.size[1] > 2000:
-                #    tmp_file.close()
-                #    data['msg'] = '图片长宽必须在250px~2000px之间'
-                    # 尺寸不对直接返回
-                #    json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-                #    self.write(json_result)
-                #    return
-                # 进行存储
-                # 指定存储目录，产生新的文件名
-                # 获取文件格式，用PIL获得的format不一定正确，所以用原文件名获得
-                image_path = "static/"                                     # 指定文件存储路径
-                image_format = send_file['filename'].split('.').pop().lower() # 得到后缀
-                tmp_name = image_path + str(int(time.time())) + '.' + image_format  # 文件名
-                image_one.save(tmp_name)
-                #关闭临时文件，关闭后临时文件自动删除
-                tmp_file.close()
+        data = {'status':101, }
+        # 构造修改语句
+        sql = "update `ofUser` set birthday=?, height=?, weight=? where username=? "
+        db.update(sql, birthday, height, weight, username)
 
-                # 先从数据库中取出旧文件地址，删除旧文件
-                sql = "select avatar from `ofUser` where username=? "
-                res = db.select_one(sql, username)
-                if res and res['avatar']:
-                    old_file = res['avatar'].encode("utf8")
-                    if os.path.isfile(old_file):  # 删除旧文件
-                        os.remove(old_file)
-
-                # 将新文件地址存入数据库
-                sql = "update `ofUser` set avatar=? where username=? "  
-                # 修改
-                res = db.update(sql, tmp_name, username)
-                if res :       # 修改成功
-                    data['status'] = True
-                    data['msg']    = '修改成功'
-            # 满足要求的图片 判断结束
-        # 用户上传图片成功 判断结束
-        json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-        self.write(json_result)
-
-class GetAvatarHandler(tornado.web.RequestHandler):
-    def post(self):
-        # 获取cookie保证用户已登录
-        username = self.get_secure_cookie("username")
-        # 检查是否登录
-        if username is None:
-            data = {'status':False, 'msg':'您尚未登录'}
-            json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-            self.write(json_result)
-            return
-        # 默认为修改失败
-        data = {'status':False, 'msg':'获取失败'}
-        # 从数据库中取出头像地址
-        sql = "select avatar from `ofUser` where username=? "
-        res = db.select_one(sql, username)
-        if res and res['avatar']:
-            avatar = res['avatar'].encode("utf8")
-            data['avatar'] = avatar
-            data['status'] = True
-            data['msg']    = '获取成功'
-        json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-        self.write(json_result)
-
-
-class UploadRecordHandler(tornado.web.RequestHandler):
-    def post(self):
-        # 获取cookie保证用户已登录
-        username = self.get_secure_cookie("username")
-        # 检查是否登录
-        if username is None:
-            data = {'status':False, 'msg':'您尚未登录'}
-            json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-            self.write(json_result)
-            return
-        # 获取post数据
-        recordDate = self.get_argument('recordDate', '')
-        duration   = int(self.get_argument('duration', '0'))
-        distance   = float(self.get_argument('distance', '0'))
-        calorie    = int(self.get_argument('calorie', '0'))
-        target     = float(self.get_argument('target', '0'))
-        data = {'status':False, 'msg':''}
-        sql = "select id from `ofRecord` where username=? and recordDate=?"
-        res = db.select_one(sql, username, recordDate)
-        if res is not None :                # 已上传过，更新信息
-            recordID = res['id']
-            sql = "update `ofRecord` set recordDate=?, duration=?, distance=?, calorie=?, target=? where id=? "
-            res = db.update(sql, recordDate, duration, distance, calorie, target, recordID)
-        else :                              # 未上传过，插入信息
-            table = "ofRecord"
-            kw = {"username":username, "recordDate":recordDate, "duration":duration, "distance":distance, "calorie":calorie, "target":target}
+        sql = "select * from `ofQuestionResult` where username=? and date=? "
+        res = db.select_one(sql, username, date)
+        # 已有，则覆盖
+        if res:
+            sql = "update `ofQuestionResult` set birthday=?, height=?, weight=?, result=? where username=? and date=? "
+            res = db.update(sql, birthday, height, weight, result, username, date)
+        # 没有，则插入
+        else:
+            table = "ofQuestionResult"
+            kw = {"username":username, "birthday":birthday, "height":height, "weight":weight, "date":date, "result":result}
             res = db.insert(table, **kw)
-        if res :       # 修改成功
-            data['status'] = True
-            data['msg']    = '修改成功'
+        data['status'] = 100
         json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
         self.write(json_result)
 
 
-# 获取用户某一天的记录
-class SingleDayRecordHandler(tornado.web.RequestHandler):
+class GetLastQuestionResultHandler(tornado.web.RequestHandler):
     def post(self):
-        username = self.get_secure_cookie("username")
-        # 检查是否登录
-        if username is None:
-            data = {'status':False, 'msg':'您尚未登录'}
-            json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-            self.write(json_result)
-            return
-        # 获取post数据
-        recordDate = self.get_argument('historyDate', '')
-        data = {'status':False, 'msg':'该日期无记录'}
-        sql = "select * from `ofRecord` where username=? and recordDate=? "
-        res = db.select_one(sql, username, recordDate)
-        if res :                         # 查找成功
-            data['status'] = True
-            data['msg']    = '获取成功'
-            info = {}
-            info['username'] = username
-            # int值不需要编码
-            info['recordDate']    = res['recordDate'].encode("utf8")
-            info['duration']      = res['duration']
-            info['distance']      = res['distance']
-            info['calorie']       = res['calorie']
-            info['target']        = res['target']
-            data['info']     = info
-        json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-        self.write(json_result)
-
-
-class TotalRecordHandler(tornado.web.RequestHandler):
-    def post(self):
-        username = self.get_secure_cookie("username")
-        # 检查是否登录
-        if username is None:
-            data = {'status':False, 'msg':'您尚未登录'}
-            json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
-            self.write(json_result)
-            return
-        data = {'status':False, 'msg':'无记录'}
-        sql = "select sum(duration), sum(distance), sum(calorie) from `ofRecord` where username=? "
+        username = self.get_argument('email', '')
+        data = {'status':101 }
+        sql = "select * from `ofQuestionResult` where username=? order by date DESC limit 1"
         res = db.select_one(sql, username)
-        # 若查询不到消息，res为：{'sum(calorie)': None, 'sum(distance)': None, 'sum(duration)': None}
-        if res['sum(calorie)']:                         # 查找成功
-            data['status'] = True
-            data['msg']    = '获取成功'
+        if res :                         # 查找成功
+            data['status'] = 100
             info = {}
             info['username'] = username
-            # int值不需要编码
-            info['totalDuration']      = int(res['sum(duration)'])
-            info['totalDistance']      = float(res['sum(distance)'])
-            info['totalCalorie']       = int(res['sum(calorie)'])
+            if res['birthday']:
+                info['birthday'] = res['birthday']
+            if res['height']:
+                info['height']   = res['height']
+            if res['weight']:
+                info['weight']   = res['weight']
+            if res['date']:
+                info['date']     = res['date']
+            if res['result']:
+                info['result']   = res['result']
             data['info']     = info
+        else:
+            data['status'] = 104
         json_result = json.dumps(data , ensure_ascii=False)     # 把python对象编码成json格式的字符串
         self.write(json_result)
+
--- skea/server.py
+++ skea/server.py
@@ -11,23 +11,19 @@ import tornado.web
 from handler import *
 
 from tornado.options import define, options
-define("port", default=8001, help="run on the given port", type=int)
+define("port", default=8002, help="run on the given port", type=int)
 
-#settings = {
+settings = {
 #        "static_path": os.path.join(os.path.dirname(__file__), "static"),
-#    }
+    }
 
 application = tornado.web.Application([
-    (r"/register",             RegisterHandler),
-    (r"/login",                LoginHandler),
-    (r"/logout",               LogoutHandler),
-    (r"/editNickname",         EditNicknameHandler),
-    (r"/getInfo",              GetInfoHandler),
-#    (r"/changeavatar",         ChageAvatarHandler),
-#    (r"/getavatar",            GetAvatarHandler),
-#    (r"/uploadGameRecord",     UploadRecordHandler),
-#    (r"/getSingleDayHistory",  SingleDayRecordHandler),
-#    (r"/getTotalHistory",      TotalRecordHandler),
+    (r"/register",              RegisterHandler),
+    (r"/login",                 LoginHandler),
+    (r"/editNickname",          EditNicknameHandler),
+    (r"/getInfo",               GetInfoHandler),
+    (r"/saveQuestionResult",    SaveQuestionResultHandler),
+    (r"/getLastQuestionResult", GetLastQuestionResultHandler),
 
 ], **settings)
 
@@ -35,7 +31,7 @@ application = tornado.web.Application([
 
 if __name__ == "__main__":
     # db.create_engine('root', 'root', 'taku')  # 本地测试使用
-    db.create_engine('root', 'root', 'skea')
+    db.create_engine('root', 'Linkcube2013', 'skea')
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
